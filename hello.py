5f45e1ad467bb1eddc275663f73378beef704940
shahadalyahya04 <shahadalyahya04@gmail.com>
2026-09-20 23:56:54 +0300

    challenge lab2
    Fix typo in sys.argv to correctly retrieve command line argument.

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import sys
 import platform
 
 def main():
+    name = sys.arfv[1] if len(sys.arfv) > 1 else "Person"
     print("Hello from Python!")
     print(f"Python version: {sys.version}")
     print(f"Running on: {platform.system()}")
