d00508c75a84ea4ea13c6b46c4d12ee684f498c4
shahadalyahya04 <shahadalyahya04@gmail.com>
2026-09-21 00:02:07 +0300

    Fix

--- hello.py
+++ hello.py
@@ -2,11 +2,11 @@ import sys
 import platform
 
 def main():
-    name = sys.arfv[1] if len(sys.arfv) > 1 else "Person"
-    print("Hello from Python!")
+    name = sys.argv[1] if len(sys.argv) > 1 else "stranger"
+
+    print(f"Hello, {name}!")
     print(f"Python version: {sys.version}")
     print(f"Running on: {platform.system()}")
 
 if __name__ == "__main__":
     main()
-    
